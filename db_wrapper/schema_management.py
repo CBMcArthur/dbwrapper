52104d0d313b4f23e21b5fe968a70beaa68825df
Christian <CBMcArthur2@gmail.com>
2024-01-21 06:44:31 -0600

    Refactoring tests and improvements to schema management module

--- db_wrapper/schema_management.py
+++ db_wrapper/schema_management.py
@@ -1,78 +1,168 @@
 from libraries import logging_utils
-try:
-    from query_execution import execute_query
-except ImportError:
-    from db_wrapper.query_execution import execute_query
+from sqlalchemy.exc import InternalError
+from db_wrapper.exceptions import DatabaseErrorException, DatabaseWarningException
 
 logger = logging_utils.get_logger(__name__)
 
 
-def schema_exists(connection, schema_name):
-    """
-    Check to see if the specified schema already exists
-    :param connection:
-    :param schema_name:
-    :return:
-    """
-    sql = f"SELECT 1 exists FROM information_schema.schemata WHERE schema_name = '{schema_name}'"
-    result = execute_query(connection, sql)
-    if len(result) > 0:
-        return bool(result[0])
-    else:
+class SchemaManager:
+    def __init__(self, dbwrapper=None, schema_name=None):
+        logging_utils.configure_logging()
+        self.logger = logging_utils.get_logger(__name__)
+        self.dbwrapper = dbwrapper
+        self.schema_name = schema_name
+
+    def schema_exists(self):
+        try:
+            exists = schema_exists(dbwrapper=self.dbwrapper, schema_name=self.schema_name)
+        except DatabaseErrorException as e:
+            self.logger.error(str(e))
+            return False
+        return exists
+
+    def schema_has_objects(self):
+        try:
+            has_objects = schema_has_objects(dbwrapper=self.dbwrapper, schema_name=self.schema_name)
+        except DatabaseErrorException as e:
+            self.logger.error(str(e))
+            return False
+        return has_objects
+
+    def create_schema(self, recreate=False, cascade=True):
+        try:
+            response = create_schema(dbwrapper=self.dbwrapper, schema_name=self.schema_name, recreate=recreate, cascade=cascade)
+            return response
+        except DatabaseErrorException as e:
+            self.logger.error(str(e))
+        except DatabaseWarningException as e:
+            self.logger.warning(str(e))
         return False
 
+    def rename_schema(self, new_schema_name):
+        try:
+            response = rename_schema(dbwrapper=self.dbwrapper, old_name=self.schema_name, new_name=new_schema_name)
+        except DatabaseErrorException as e:
+            self.logger.error(str(e))
+            return False
+        except DatabaseWarningException as e:
+            self.logger.warning(str(e))
+            return False
+        if response is True:
+            self.schema_name = new_schema_name
+        return response
 
-def create_schema(connection, schema_name):
+    def delete_schema(self, cascade=False):
+        try:
+            response = delete_schema(dbwrapper=self.dbwrapper, schema_name=self.schema_name, cascade=cascade)
+        except DatabaseErrorException as e:
+            self.logger.error(str(e))
+            return False
+        except DatabaseWarningException as e:
+            self.logger.error(str(e))
+            return False
+        return response
+
+
+def schema_exists(dbwrapper, schema_name):
     """
-    Create a new schema in the db_engine's PostgreSQL database
-    :param connection:
+    Check to see if the specified schema already exists
+    :param dbwrapper: Database wrapper object with an execute_query method
+    :param schema_name: Name of the schema to check
+    :return: True if the schema exists, False otherwise
+    """
+    sql = "SELECT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = :schema_name)"
+    try:
+        result = dbwrapper.execute_query(sql, schema_name=schema_name)
+        return bool(result[0]['exists'])
+    except Exception as e:
+        error_msg = f"Error checking if schema, {schema_name}, exists: {e}"
+        raise DatabaseErrorException(error_msg, sql)
+
+
+def schema_has_objects(dbwrapper, schema_name):
+    """
+    Check to see if schema has any tables
+    :param dbwrapper:
     :param schema_name:
     :return:
     """
-    if schema_exists(connection, schema_name):
-        error_msg = f"Error: The schema specified, {schema_name}, already exists. Creation skipped."
-        logger.warning(error_msg)
-        return error_msg
+    sql = f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = :schema_name LIMIT 1);"
+    try:
+        result = dbwrapper.execute_query(sql, schema_name=schema_name)
+        return bool(result[0]['exists'])
+    except Exception as e:
+        error_msg = f"Error checking if schema, {schema_name}, has objects: {e}"
+        raise DatabaseErrorException(error_msg, sql)
+
+
+def create_schema(dbwrapper, schema_name, recreate=False, cascade=True):
+    """
+    Create a new schema in the db_engine's PostgreSQL database
+    :param dbwrapper:
+    :param schema_name:
+    :param recreate:
+    :param cascade:
+    :return:
+    """
+    if schema_exists(dbwrapper, schema_name):
+        if not recreate:
+            error_msg = f"The schema specified, {schema_name}, already exists. Creation skipped."
+            raise DatabaseWarningException(error_msg)
+        delete_schema(dbwrapper, schema_name, cascade)
 
     sql = f"CREATE SCHEMA IF NOT EXISTS {schema_name}"
-    result = execute_query(connection, sql)
-    return result
+    try:
+        result = dbwrapper.execute_query(sql)
+        return result
+    except Exception as e:
+        error_msg = f"Error creating schema, {schema_name}: {e}"
+        raise DatabaseErrorException(error_msg, sql)
 
 
-def rename_schema(connection, old_name, new_name):
+def rename_schema(dbwrapper, old_name, new_name):
     """
     Alter an existing schema to rename it.
-    :param connection:
+    :param dbwrapper:
     :param old_name:
     :param new_name:
     :return:
     """
-    if not schema_exists(connection, old_name):
-        error_msg = f"Error: The schema specified, {old_name}, does not exist.  Renaming skipped."
-        logger.error(error_msg)
-        return error_msg
-    if schema_exists(connection, new_name):
-        error_msg = f"Error: The new schema name, {new_name}, already exists. Renaming skipped."
-        logger.error(error_msg)
-        return error_msg
+    if not schema_exists(dbwrapper, old_name):
+        error_msg = f"The schema, {old_name}, does not exist.  Renaming skipped."
+        raise DatabaseErrorException(error_msg)
+    if schema_exists(dbwrapper, new_name):
+        error_msg = f"The new schema, {new_name}, already exists. Renaming skipped."
+        raise DatabaseErrorException(error_msg)
 
     sql = f"ALTER SCHEMA {old_name} RENAME TO {new_name}"
-    result = execute_query(connection, sql)
+    result = dbwrapper.execute_query(sql)
     return result
 
-def delete_schema(connection, schema_name, cascade=True):
+def delete_schema(dbwrapper, schema_name, cascade=False):
     """
     Delete the specified schema
-    :param connection:
+    :param dbwrapper:
     :param schema_name: Schema to delete
     :param cascade: Also delete any tables or other objects in the schema
     :return:
     """
-    if not schema_exists(connection, schema_name):
-        error_msg = f"Error: The schema specified, {schema_name}, does not exist.  Cannot delete."
-        logger.error(error_msg)
-        return error_msg
+    if not schema_exists(dbwrapper, schema_name):
+        error_msg = f"The schema, {schema_name}, does not exist.  Cannot delete."
+        raise DatabaseWarningException(error_msg)
+
+    if cascade is False:
+        if schema_has_objects(dbwrapper, schema_name):
+            error_msg = f"The schema, {schema_name}, cannot be deleted when tables exist in it unless cascade is True"
+            raise DatabaseErrorException(error_msg)
 
     sql = f"DROP SCHEMA IF EXISTS {schema_name} {'CASCADE' if cascade else ''}"
-    result = execute_query(connection, sql)
-    return result
+    try:
+        result = dbwrapper.execute_query(sql)
+        return result
+    except InternalError as e:
+        if 'DependentObjectsStillExist' in str(e):
+            error_msg = f"The schema, {schema_name}, has tables or other objects in it. Either drop the objects or use the cascade option"
+            raise DatabaseErrorException(error_msg)
+        else:
+            error_msg = f"A problem occurred dropping the schema, {schema_name}"
+            raise DatabaseErrorException(error_msg)
---
+++ tests/db_wrapper/test_schema_management.py
@@ -0,0 +1,198 @@
+import pytest
+import uuid
+
+from unittest.mock import Mock, patch
+from db_wrapper.db_wrapper import DBWrapper
+from db_wrapper.exceptions import DatabaseErrorException, DatabaseWarningException
+from db_wrapper import schema_management
+
+
+class TestSchema:
+    @pytest.fixture
+    def dbwrapper(self):
+        return Mock()
+
+    @pytest.mark.parametrize('schema_name, expected_result', [
+        ("existing_schema", True),  # Test when schema exists
+        ("nonexisting_schema", False),  # Test when schema does not exist
+    ])
+    def test_schema_exists(self, dbwrapper, schema_name, expected_result):
+        dbwrapper.execute_query.return_value = [{'exists': True}] if expected_result is True else [{'exists': False}]
+        result = schema_management.schema_exists(dbwrapper, schema_name)
+        assert result == expected_result
+        dbwrapper.execute_query.assert_called_once_with('SELECT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = :schema_name)', schema_name=schema_name)
+
+    def test_schema_exists_exception(self, dbwrapper):
+        dbwrapper.execute_query.side_effect = Exception("Test error")
+        with pytest.raises(DatabaseErrorException):
+            schema_management.schema_exists(dbwrapper, 'some schema')
+
+    def test_schema_exists_class(self, dbwrapper):
+        with patch('db_wrapper.schema_management.schema_exists') as mock_schema_exists:
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+
+            mock_schema_exists.side_effect = [True, False, DatabaseErrorException('Error checking schema')]
+
+            assert schema_manager.schema_exists() is True
+            assert schema_manager.schema_exists() is False
+            assert schema_manager.schema_exists() is False
+
+            mock_schema_exists.assert_called_with(dbwrapper=dbwrapper, schema_name='test_schema')
+
+
+    @pytest.mark.parametrize('schema_name, has_objects, expected_result', [
+        ("schema_with_objects", True, True),
+        ("schema_without_objects", False, False)
+    ])
+    def test_schema_has_objects(self, dbwrapper, schema_name, has_objects, expected_result):
+        dbwrapper.execute_query.return_value = [{'exists': True}] if expected_result is True else [{'exists': False}]
+        result = schema_management.schema_has_objects(dbwrapper, schema_name)
+        assert result == expected_result
+        dbwrapper.execute_query.assert_called_once_with("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = :schema_name LIMIT 1);", schema_name=schema_name)
+
+    def test_schema_has_objects_exception(self, dbwrapper):
+        dbwrapper.execute_query.side_effect = Exception("Test error")
+        with pytest.raises(DatabaseErrorException):
+            schema_management.schema_has_objects(dbwrapper, 'some schema')
+
+    def test_schema_has_objects_class(self, dbwrapper):
+        with patch('db_wrapper.schema_management.schema_has_objects') as mock_schema_has_objects:
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+
+            mock_schema_has_objects.side_effect = [True, False, DatabaseErrorException("Error checking schema")]
+            assert schema_manager.schema_has_objects() is True
+            assert schema_manager.schema_has_objects() is False
+            assert schema_manager.schema_has_objects() is False
+
+            mock_schema_has_objects.assert_called_with(dbwrapper=dbwrapper, schema_name='test_schema')
+
+    @pytest.mark.parametrize("schema_name, recreate, cascade, schema_exists, objects_exists, expected_warning, expected_error", [
+        ('existing schema', False, True, True, True, DatabaseWarningException, None),  # Existing schema with object, but don't recreate = Warning
+        ('existing schema', True, False, True, True, None, DatabaseErrorException),  # Existing schema with object, recreate without cascade = warning
+        ('existing schema', True, True, True, True, None, None),  # Existing schema with object, recreate with cascade = success
+        ('existing schema', True, True, True, False, None, None),  # Existing schema without object, recreate = success
+        ('existing schema', True, False, True, False, None, None),  # Existing schema without object, recreate = success
+        ('nonexisting', False, True, False, False, None, None)  # Nonexisting schema create = success
+    ])
+    @patch('db_wrapper.schema_management.schema_exists')
+    @patch('db_wrapper.schema_management.delete_schema')
+    def test_create_schema(self, mock_delete_schema, mock_schema_exists, dbwrapper,
+                           schema_name, recreate, cascade, schema_exists, objects_exists, expected_warning, expected_error):
+        mock_schema_exists.return_value = schema_exists
+        dbwrapper.execute_query.return_value = True if expected_warning is None else False
+
+        if schema_exists and objects_exists and recreate and not cascade:
+            mock_delete_schema.side_effect = DatabaseErrorException("Error deleting schema")
+
+        if expected_warning:
+            with pytest.raises(expected_warning):
+                schema_management.create_schema(dbwrapper, schema_name, recreate, cascade)
+        elif expected_error:
+            with pytest.raises(expected_error):
+                schema_management.create_schema(dbwrapper, schema_name, recreate, cascade)
+        else:
+            result = schema_management.create_schema(dbwrapper, schema_name, recreate, cascade)
+            assert result is True
+            dbwrapper.execute_query.assert_called_once_with(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
+
+    @patch('db_wrapper.schema_management.schema_exists')
+    @patch('db_wrapper.schema_management.delete_schema')
+    def test_create_schema_exception(self, mock_delete_schema, mock_schema_exists, dbwrapper):
+        mock_schema_exists.return_value = False
+        dbwrapper.execute_query.side_effect = Exception("Test error")
+        with pytest.raises(DatabaseErrorException):
+            schema_management.create_schema(dbwrapper, 'some schema', True, True)
+
+    def test_create_schema_class(self, dbwrapper):
+        with patch('db_wrapper.schema_management.create_schema') as mock_create_schema:
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+
+            mock_create_schema.side_effect = [True, False, DatabaseErrorException("Error creating"), DatabaseWarningException("Warning creating")]
+            assert schema_manager.create_schema() is True
+            assert schema_manager.create_schema() is False
+            assert schema_manager.create_schema() is False
+            assert schema_manager.create_schema() is False
+
+            mock_create_schema.assert_called_with(dbwrapper=dbwrapper, schema_name='test_schema', recreate=False, cascade=True)
+
+
+    @pytest.mark.parametrize("schema_name, new_name, exception_thrown", [
+        ('missing_schema', 'renamed_schema', DatabaseErrorException),  # DatabaseErrorException: renaming a schema that doesn't exist
+        ('schema_name', 'already_used', DatabaseErrorException),  # DatabaseErrorException: new name is already in use
+        ('schema_name', "renamed_schema", None)
+    ])
+    @patch('db_wrapper.schema_management.schema_exists')
+    def test_rename_schema(self, mock_schema_exists, dbwrapper, schema_name, new_name, exception_thrown):
+        captured_schema_name = None
+        def schema_exists_side_effect(self, name):
+            nonlocal captured_schema_name
+            captured_schema_name = name
+            return False if name == 'missing_schema' or name == 'renamed_schema' else True
+        mock_schema_exists.side_effect = schema_exists_side_effect
+        dbwrapper.execute_query.side_effect = [
+            DatabaseErrorException("Error renaming schema") if exception_thrown == DatabaseErrorException else True
+        ]
+
+        if exception_thrown:
+            with pytest.raises(exception_thrown):
+                schema_management.rename_schema(dbwrapper, schema_name, new_name)
+        else:
+            result = schema_management.rename_schema(dbwrapper, schema_name, new_name)
+            assert result is True
+            dbwrapper.execute_query.assert_called_once_with(f"ALTER SCHEMA {schema_name} RENAME TO {new_name}")
+
+    def test_rename_schema_class(self, dbwrapper):
+        with patch('db_wrapper.schema_management.rename_schema') as mock_rename_schema:
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+            mock_rename_schema.side_effect = [True]
+            assert schema_manager.rename_schema('new_name') is True
+            assert schema_manager.schema_name == 'new_name'
+
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+            mock_rename_schema.side_effect = [False]
+            assert schema_manager.rename_schema('new_name') is False
+
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+            mock_rename_schema.side_effect = [DatabaseErrorException('Testing error')]
+            assert schema_manager.rename_schema('new_name') is False
+
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+            mock_rename_schema.side_effect = [DatabaseWarningException('Testing Warning')]
+            assert schema_manager.rename_schema('new_name') is False
+
+            mock_rename_schema.assert_called_with(dbwrapper=schema_manager.dbwrapper, old_name='test_schema', new_name='new_name')
+
+
+    @pytest.mark.parametrize("schema_name, cascade, schema_exists, has_objects, expected_exception", [
+        ('existing', False, True, False, None),  # Existing with no objects = success
+        ('existing', False, True, True, DatabaseErrorException),  # Existing with objects, no cascade = error
+        ('existing', True, True, True, None),  # Existing with objects, with cascade = success
+        ('nonexisting', False, False, False, DatabaseWarningException),  # Non-existing schema = warning
+    ])
+    @patch('db_wrapper.schema_management.schema_exists')
+    @patch('db_wrapper.schema_management.schema_has_objects')
+    def test_delete_schema(self, mock_schema_has_objects, mock_schema_exists, dbwrapper,
+                           schema_name, cascade, schema_exists, has_objects, expected_exception):
+        mock_schema_has_objects.return_value = has_objects
+        mock_schema_exists.return_value = schema_exists
+        dbwrapper.execute_query.return_value = True if expected_exception is None else False
+
+        if expected_exception:
+            with pytest.raises(expected_exception):
+                schema_management.delete_schema(dbwrapper, schema_name, cascade)
+        else:
+            result = schema_management.delete_schema(dbwrapper, schema_name, cascade)
+            assert result is True
+            dbwrapper.execute_query.assert_called_once_with(f"DROP SCHEMA IF EXISTS {schema_name} {'CASCADE' if cascade else ''}")
+
+    def test_delete_schema_class(self, dbwrapper):
+        with patch('db_wrapper.schema_management.delete_schema') as mock_delete_schema:
+            schema_manager = schema_management.SchemaManager(dbwrapper, 'test_schema')
+            mock_delete_schema.side_effect = [False, DatabaseErrorException('Test Error'), DatabaseWarningException('Test Warning'), True]
+
+            assert schema_manager.delete_schema() is False
+            assert schema_manager.delete_schema() is False
+            assert schema_manager.delete_schema() is False
+            assert schema_manager.delete_schema() is True
+
+            mock_delete_schema.assert_called_with(dbwrapper=dbwrapper, schema_name='test_schema', cascade=False)
